6f0f47a23bf8cbd78040a4ea0ff2f038062449fd
raghav patel <patelraghav014@gmail.com>
2024-03-19 15:32:27 +0530

    data ingestion added

---
+++ app.py
@@ -0,0 +1,3 @@
+from hate.pipeline.train_pipeline import TrainPipeline
+obj=TrainPipeline()
+obj.run_pipeline()
---
+++ hate/constants/__init__.py
@@ -0,0 +1,20 @@
+import os
+
+
+from datetime import datetime
+
+ARTIFACTS_DIR: str = "artifacts"
+LABEL = 'label'
+TWEET = 'tweet'
+
+
+"""
+Data Ingestion related constant 
+"""
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+
+DATA_DOWNLOAD_URL: str = "https://github.com/raghavpatel2507/Hate_Speech_Classification/raw/main/data/dataset.zip"
+
+
